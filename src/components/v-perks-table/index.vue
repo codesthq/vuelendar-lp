5d8ed7cf47904fed85ae3c67b0f559fc8b03df08
Mateusz Staniuk <staniuk.mateusz@gmail.com>
2019-08-27 09:31:56 +0200

    Add perks table component

---
+++ src/components/v-perks-table/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="perks-table">
+    <div class="perks-table__item">
+      <div class="perks-table__icon">
+        <VSvgIcon icon="puzzle" />
+      </div>
+      <div class="perks-table__caption">
+        Easy to use
+      </div>
+    </div>
+    <div class="perks-table__item">
+      <div class="perks-table__icon">
+        <VSvgIcon icon="scale" />
+      </div>
+      <div class="perks-table__caption">
+        Extendable
+      </div>
+    </div>
+    <div class="perks-table__item">
+      <div class="perks-table__icon">
+        <VSvgIcon icon="balance" />
+      </div>
+      <div class="perks-table__caption">
+        Optimized
+      </div>
+    </div>
+    <div class="perks-table__item">
+      <div class="perks-table__icon">
+        <VSvgIcon icon="tag" />
+      </div>
+      <div class="perks-table__caption perks-table__caption--mark">
+        Totally free!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VSvgIcon from '@components/v-svg-icon';
+
+export default {
+  name: 'VPerksTable',
+  components: {
+    VSvgIcon,
+  },
+};
+</script>
+
+<style lang="scss" src="./v-perks-table.scss" />

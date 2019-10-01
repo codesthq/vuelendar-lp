c48fdf79883cae84f63603a0a3fcd6f1891c5fa5
Mateusz Staniuk <staniuk.mateusz@gmail.com>
2019-10-01 12:55:31 +0200

    Document range selector is-disabled

--- src/views/Docs/index.vue
+++ src/views/Docs/index.vue
@@ -173,8 +173,7 @@
                       &lt;v-day-selector v-model="date" disabled-dates="{
                         to: '2019-04-21',
                       }" /&gt;
-                    </pre
-                      >
+                      </pre>
                     </highlight-code>
                   </template>
                 </VSection>
@@ -182,27 +181,68 @@
             </VPane>
             <VPane>
               <template v-slot:title>
-                <a id="range-selector" href="#range-selector">range selector</a>
+                <a id="range-selector" href="#range-selector">Range selector</a>
               </template>
               <template v-slot:default>
                 <VSection>
                   <template v-slot:title>
-                    <a id="range-selector-usage" href="#range-selector-usage"
-                      >Usage</a
-                    >
+                    <a id="range-selector-usage" href="#range-selector-usage">
+                      Usage
+                    </a>
                   </template>
                   <template v-slot:default>
-                    [TBA]
+                    <h4>Register component</h4>
+                    <highlight-code lang="javascript">
+                      <pre>
+                        import VRangeSelector from 'vuelendar/components/vl-range-selector';
+                        export default {
+                          components: {
+                            VDaySelector
+                          },
+                          data () {
+                            return { range: {} }
+                          }
+                        }
+                      </pre>
+                    </highlight-code>
+
+                    <h4>Use in template</h4>
+                    <highlight-code lang="vue">
+                      <pre>
+                        &lt;v-range-selector
+                          :start-date.sync=&quot;range.start&quot;
+                          :end-date.sync=&quot;range.end&quot;
+                        /&gt;
+                      </pre>
+                    </highlight-code>
                   </template>
                 </VSection>
                 <VSection>
                   <template v-slot:title>
-                    <a id="range-selector-props" href="#range-selector-props"
-                      >Props</a
-                    >
+                    <a id="range-selector-props" href="#range-selector-props">
+                      Props
+                    </a>
                   </template>
                   <template v-slot:default>
-                    [TBA]
+                    <h4>is-disabled</h4>
+                    In range selector date disabling is achieved by providing
+                    function to is disabled prop. Function should accept one
+                    string argument containing date to be tested in YYYY-MM-DD
+                    format.<br />
+                    YYYY-MM-DD format is has it's practical benefits since it
+                    since it can be easily compared.
+
+                    <highlight-code lang="vue">
+                      <pre>
+                      &lt;v-range-selector
+                        :start-date.sync=&quot;range.start&quot;
+                        :end-date.sync=&quot;range.end&quot;
+                        :is-disabled=&quot;date =&gt;date > '2020-01-01'&quot;
+                      /&gt;
+                      </pre>
+                    </highlight-code>
+                    In this example dates later than 1st of January 2020 will be
+                    disabled.
                   </template>
                 </VSection>
               </template>
